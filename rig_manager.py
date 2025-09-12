ca9e81ab4179bee74942e38fc1de02f040a5b851
mislavskii <jornjat@gmail.com>
2025-09-12 10:07:48 +0700

    implemented core throttle, enabled VCS

---
+++ rig_manager.py
@@ -0,0 +1,34 @@
+import json
+import random
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class RigManager:
+
+    def __init__(self, config_path=''):
+        self.config_path = config_path if config_path else os.getenv('DEFAULT_CONFIG_PATH', '')
+        with open(self.config_path, 'r') as file:
+            self.config_dict = json.load(file)
+
+    def set_ncores(self, n):
+        all_cores = self._prepare_core_list()
+        cores_to_use = sorted(random.sample(all_cores, n))
+        print(cores_to_use)
+        self.config_dict['cpu']['rx'] = cores_to_use
+        try:
+            with open(self.config_path, 'w') as file:
+                json.dump(self.config_dict, file)
+        except OSError as e:
+            print(e)
+
+    def _prepare_core_list(self) -> list[int]:
+        return self.config_dict['cpu']['rx/wow']
+    
+
+if __name__ == '__main__':
+    mgr = RigManager()
+    mgr.set_ncores(6)
